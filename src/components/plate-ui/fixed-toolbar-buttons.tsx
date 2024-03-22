8cbfc7977dc400938c2ce3f8dfd748358c113309
eglee <eg.lee@spendit.kr>
2024-03-22 10:45:10 +0900

    init: 기본 기능 구현 II

--- src/components/plate-ui/fixed-toolbar-buttons.tsx
+++ src/components/plate-ui/fixed-toolbar-buttons.tsx
@@ -6,7 +6,15 @@ import {
     MARK_STRIKETHROUGH, MARK_SUBSCRIPT, MARK_SUPERSCRIPT,
     MARK_UNDERLINE,
 } from '@udecode/plate-basic-marks';
-import {focusEditor, insertEmptyElement, toggleNodeType, useEditorReadOnly, useEditorRef} from '@udecode/plate-common';
+import {
+    ELEMENT_DEFAULT,
+    focusEditor,
+    insertEmptyElement,
+    insertNodes,
+    toggleNodeType,
+    useEditorReadOnly,
+    useEditorRef
+} from '@udecode/plate-common';
 
 import {Icons, iconVariants} from '../icons';
 
@@ -14,7 +22,7 @@ import { MarkToolbarButton } from './mark-toolbar-button';
 import { ModeDropdownMenu } from './mode-dropdown-menu';
 import {ToolbarButton, ToolbarGroup} from './toolbar';
 import {FontSizeDropDownMenu} from "./font-size-dropdown-menu";
-import {MARK_BG_COLOR, MARK_COLOR, MARK_FONT_SIZE} from "@udecode/plate-font";
+import {MARK_BG_COLOR, MARK_COLOR} from "@udecode/plate-font";
 import {ColorDropdownMenu} from "./color-dropdown-menu";
 import {AlignDropdownMenu} from "./align-dropdown-menu";
 import {LineHeightDropdownMenu} from "./line-height-dropdown-menu";
@@ -42,11 +50,11 @@ export function FixedToolbarButtons() {
           <>
             <ToolbarGroup noSeparator>
                 {/*요소를 추가하는 드롭다운 버튼*/}
-              <InsertDropdownMenu />
+              {/*<InsertDropdownMenu />*/}
               <FontSizeDropDownMenu />
             </ToolbarGroup>
 
-              <ToolbarGroup>
+              <ToolbarGroup noSeparator>
                   <MarkToolbarButton tooltip="굵게" nodeType={MARK_BOLD}>
                       <span style={{fontWeight: 'bold'}}>가</span>
                   </MarkToolbarButton>
@@ -89,7 +97,7 @@ export function FixedToolbarButtons() {
                   </MarkToolbarButton>
               </ToolbarGroup>
 
-              <ToolbarGroup>
+              <ToolbarGroup noSeparator>
                   <AlignDropdownMenu/>
                   <LineHeightDropdownMenu/>
                   <IndentListToolbarButton nodeType={ListStyleType.Disc}/>
@@ -98,59 +106,71 @@ export function FixedToolbarButtons() {
                   <IndentToolbarButton/>
                   <MarkToolbarButton tooltip="구분선" nodeType={ELEMENT_HR} onClick={() => {
                       toggleNodeType(editor, { activeType: ELEMENT_HR });
-                      insertEmptyElement(editor, ELEMENT_PARAGRAPH);
+                      insertNodes(editor, {
+                          type: ELEMENT_DEFAULT,
+                          children: [{ text: '' }],
+                      });
                       focusEditor(editor);
                   }}>
                       구분선
                   </MarkToolbarButton>
                   <MarkToolbarButton tooltip="인용" nodeType={ELEMENT_BLOCKQUOTE} onClick={() => {
                       toggleNodeType(editor, { activeType: ELEMENT_BLOCKQUOTE });
-                      // insertEmptyElement(editor, ELEMENT_PARAGRAPH);
+                      // insertNodes(editor, {
+                      //     type: ELEMENT_DEFAULT,
+                      //     children: [{ text: '' }],
+                      // });
                       focusEditor(editor);
                   }}>
                       인용
                   </MarkToolbarButton>
                   <LinkToolbarButton />
               </ToolbarGroup>
-            <ToolbarGroup>
-                <ToolbarButton onClick={async (event) => {
-                    await insertMedia(editor, {getUrl: () => {
-                            return new Promise((resolve, reject) => {
-                                const input = document.createElement('input');
-                                input.type = 'file';
-                                input.accept = 'image/*';
-                                // 파일 선택창 열기
-                                input.click();
-                                // 파일 선택 시 처리
-                                input.onchange = (e) => {
-                                    const files = (e.target as HTMLInputElement).files;
-                                    if (!files || files.length === 0) {
-                                        reject('No file selected');
-                                        return;
-                                    }
-                                    // 파일 읽기
-                                    const reader = new FileReader();
-                                    reader.onload = (e) => {
-                                        const result = e.target!.result as string;
-                                        resolve(result);
-                                    };
-                                    reader.readAsDataURL(files[0]);
-                                };
-                            });
-                        }, type: ELEMENT_IMAGE});
-                    insertEmptyElement(editor, ELEMENT_PARAGRAPH);
-                    focusEditor(editor);
-                }}>
-                    <Icons.image />
-                </ToolbarButton>
-                <TableDropdownMenu />
-            </ToolbarGroup>
+              <ToolbarGroup noSeparator>
+                  <TableDropdownMenu />
+                  {/*<MediaToolbarButton nodeType={ELEMENT_IMAGE} />*/}
+
+                  <ToolbarButton onClick={async (event) => {
+                      await insertMedia(editor, {getUrl: () => {
+                              return new Promise((resolve, reject) => {
+                                  const input = document.createElement('input');
+                                  input.type = 'file';
+                                  input.accept = 'image/*';
+                                  // 파일 선택창 열기
+                                  input.click();
+                                  // 파일 선택 시 처리
+                                  input.onchange = (e) => {
+                                      const files = (e.target as HTMLInputElement).files;
+                                      if (!files || files.length === 0) {
+                                          reject('No file selected');
+                                          return;
+                                      }
+                                      // 파일 읽기
+                                      const reader = new FileReader();
+                                      reader.onload = (e) => {
+                                          const result = e.target!.result as string;
+                                          resolve(result);
+                                      };
+                                      reader.readAsDataURL(files[0]);
+                                  };
+                              });
+                          }, type: ELEMENT_IMAGE});
+                      insertNodes(editor, {
+                          type: ELEMENT_DEFAULT,
+                          children: [{ text: '' }],
+                      });
+                      focusEditor(editor);
+                  }}>
+                      <Icons.image />
+                  </ToolbarButton>
+              </ToolbarGroup>
+
           </>
         )}
 
-        <ToolbarGroup noSeparator>
-            <ModeDropdownMenu/>
-        </ToolbarGroup>
+        {/*<ToolbarGroup noSeparator>*/}
+        {/*    <ModeDropdownMenu/>*/}
+        {/*</ToolbarGroup>*/}
     </div>
   );
 }
--- src/components/plate-ui/fixed-toolbar.tsx
+++ src/components/plate-ui/fixed-toolbar.tsx
@@ -4,6 +4,5 @@ import { Toolbar } from './toolbar';
 
 export const FixedToolbar = withCn(
   Toolbar,
-  // 고정시키고 싶으면 여기에 스타일링 추가하면 됨.
-  // 'supports-backdrop-blur:bg-background/60 sticky left-0 top-[57px] z-50 w-full justify-between overflow-x-auto rounded-t-lg border-b border-b-border bg-background/95 backdrop-blur'
+  'supports-backdrop-blur:bg-background/60 sticky left-0 top-[57px] z-50 w-full justify-between overflow-x-auto rounded-t-lg border-b border-b-border bg-background/95 backdrop-blur'
 );
